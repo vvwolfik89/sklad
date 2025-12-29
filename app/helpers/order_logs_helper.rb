3394c2af01c89aeed44d9ae4b94c17491e5e3503
vvwolfik89 <vv-wolf89@yandex.ru>
2025-12-30 00:36:54 +0300

    feature-12 added get_highlight_class

--- app/helpers/order_logs_helper.rb
+++ app/helpers/order_logs_helper.rb
@@ -1,2 +1,28 @@
 module OrderLogsHelper
+
+  ROUTE_DIRECTION = {
+    'Минск'    => 'highlight-minsk',
+    'Гомель'   => 'highlight-gomel',
+    'Бобруйск' => 'highlight-gomel',
+    'Шклов' => 'highlight-mogilev',
+    'Могилев' => 'highlight-mogilev',
+    'Борисов' => 'highlight-vitebsk',
+    'Витебск' => 'highlight-vitebsk',
+    'Гродно' => 'highlight-grodno',
+    'Брест' => 'highlight-brest',
+    'Барановичи' => 'highlight-brest',
+    'Солигорск' => 'highlight-soligorsk'
+    # ... другие города
+  }.freeze
+
+
+  def get_highlight_class(address)
+    ROUTE_DIRECTION.each do |city, css_class|
+      # Приводим к нижнему регистру и ищем подстроку
+      if address.downcase.include?(city.downcase)
+        return css_class
+      end
+    end
+    '' # если город не найден
+  end
 end
