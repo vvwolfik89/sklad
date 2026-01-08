dc5d1d2816ccd59c9cc55fa03ac9aad31a4d3b85
vvwolfik89 <vv-wolf89@yandex.ru>
2026-01-08 23:37:19 +0300

    feature-12 added css

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -3,21 +3,21 @@ class Partner < ApplicationRecord
 
   require 'roo'
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   # has_and_belongs_to_many :users
 
 
   def self.import_from_excel(file)
     spreadsheet = Roo::Spreadsheet.open(file.path)
-    header = spreadsheet.row(1) # первая строка — заголовки
+    header = spreadsheet.row(12) # первая строка — заголовки
 
     imported = 0
-    (2..spreadsheet.last_row).each do |i|
+    (13..spreadsheet.last_row).each do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
       partner = Partner.new(
-        name: row["name"],
-        email: row["email"],
-        phone_number: row["phone_number"]
+        name: row["Получатель"],
+        address: row["Юридический адрес"],
+        # phone_number: row["phone_number"]
       )
       imported += 1 if partner.save
     end
